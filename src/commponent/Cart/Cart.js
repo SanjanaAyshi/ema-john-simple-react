0270eb28f643437932016af8e14de2b1b44c9e7e
sanjana-ayshi <sanjana.ayshi92@gamil.com>
2022-10-03 00:16:02 +0600

    Cart updated

--- src/commponent/Cart/Cart.js
+++ src/commponent/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
-
-const Cart = (cart) => {
+import './Cart.css';
+const Cart = ({cart}) => {
+    console.log(cart);
+    let total=0;
+    let shipping=0;
+    for(const product of cart)
+    {
+        total=total + product.price;
+        shipping=shipping + product.shipping;
+    }
+    const tax= parseFloat((total*.1).toFixed(2));
+    const grandTotal= total +shipping+tax;
     return (
-        <div>
+        <div className='cart'>
          <h1>order summary:</h1>
         <p>Selected items:{cart.length}</p>
+        <p>Total Price: ${total} </p>
+        <p>Total Shipping:${shipping} </p>
+        <p>Total Tax:${tax}</p>
+        <h5>Grand Total: ${grandTotal} </h5>
         </div>
     );
 };
